3b974283c6ed372f4b08b2c1eeebd3f735e732fe
Melih Sargın <mehmetmelihsargin@gmail.com>
2023-07-18 15:36:58 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const NewsAPI = require('newsapi');
+const express = require('express');
+const app = express();
+const newsapi = new NewsAPI('b36e8013267648a091a2ec30c6bf77d5');
+
+app.get('/', (req, res) => {
+    newsapi.v2.topHeadlines({
+        category: 'business',
+        language: 'tr',
+        country: 'tr'
+    }).then(response => {
+        res.send(response);
+    }).catch(error => {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
